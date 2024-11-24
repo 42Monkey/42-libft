872ad3e1a594649fa016f1e795e8d9b0ca210799
42Monkey <fortytwoworkstation@gmail.com>
2024-11-24 20:25:27 +0700

    add character check function

--- libft.h
+++ libft.h
@@ -2,12 +2,21 @@
 # define LIBFT_H
 
 // testing
-#include <stdio.h>
-#include <ctype.h>
+# include <stdio.h>
+# include <ctype.h>
 
+// character check
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
 
-// ctype.h
-int	ft_isalpha(int c);
-
+// string manipulation
+size_t		ft_strlen(const char *s);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
