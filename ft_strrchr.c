a5f1c820e4a63842c8a7eaed1144c6547fcc51ba
42Monkey <fortytwoworkstation@gmail.com>
2024-11-24 23:16:35 +0700

    fix ft_strrchr, points to the last character of the string

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,6 +1,7 @@
 #include "libft.h"
 
 // locates character in string
+// returns pointer to last matched character or NULL if not
 char	*ft_strrchr(const char *s, int c)
 {
 	char		target;
@@ -18,7 +19,7 @@ char	*ft_strrchr(const char *s, int c)
 	}
 	if (target == '\0')
 	{
-		return ((char *)s);
+		return ((char *)s - 1);
 	}
 	return ((char *)last);
 }
@@ -26,7 +27,7 @@ char	*ft_strrchr(const char *s, int c)
 // int	main(void)
 // {
 // 	const char *string = "Hello, Bangkok!";
-// 	char c = 'B';
+// 	char c = 'o';
 
 // 	printf("strrchr: %s\n", strrchr(string, c));
 // 	printf("ft_strrchr: %s\n", ft_strrchr(string, c));
