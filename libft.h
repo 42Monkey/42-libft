5af6b73bb6581a78e2f92c28db3ba63e1704e32a
42Monkey <fortytwoworkstation@gmail.com>
2024-11-25 23:32:38 +0700

    add libft.h

--- libft.h
+++ libft.h
@@ -6,6 +6,8 @@
 # include <ctype.h>
 # include <string.h>
 # include <stdlib.h>
+# include <limits.h>
+# include <unistd.h>
 
 // character check
 int			ft_isalpha(int c);
@@ -39,5 +41,48 @@ int			ft_atoi(const char *str);
 // memory allocation
 void		*ft_calloc(size_t count, size_t size);
 char		*ft_strdup(const char *s1);
+
+// string operations
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strtrim(char const *s1, char const *set);
+char		**ft_split(char const *s, char c);
+char		*ft_itoa(int n);
+
+// string iteration
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void		ft_striteri(char *s, void (*f)(unsigned int, char *));
+
+// file descriptor
+void		ft_putchar_fd(char c, int fd);
+void		ft_putstr_fd(char *s, int fd);
+void		ft_putendl_fd(char *s, int fd);
+void		ft_putnbr_fd(int n, int fd);
+
+// bonus
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+// list creation
+t_list		*ft_lstnew(void *content);
+
+// list modification
+void		ft_lstadd_front(t_list **lst, t_list *new);
+void		ft_lstadd_back(t_list **lst, t_list *new);
+
+// list access
+int			ft_lstsize(t_list *lst);
+t_list		*ft_lstlast(t_list *lst);
+
+// list deletion
+void		ft_lstdelone(t_list *lst, void (*del)(void *));
+void		ft_lstclear(t_list **lst, void (*del)(void *));
+
+// list iteration
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+void		ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
